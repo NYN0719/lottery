edade57d53ca202ffe706bb2d9431e095811ea7f
summer128 <2864567255@qq.com>
2019-09-18 18:59:19 +0800

    twj

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Records from './views/buyrecord.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Records',
+      component: Records
     },
     {
       path: '/about',
