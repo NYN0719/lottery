05c891e5ab853b26c7c8b63dc160d6fb6feaba11
Your Name <you@example.com>
2019-09-18 19:20:02 +0800

    zx

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Records from './views/buyrecord.vue'
 import rule from './views/rule.vue'
 import index from './views/index.vue'
@@ -24,14 +23,7 @@ export default new Router({
       name: 'jifen',
       component: jifen
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+   {
       path: '/rule',
       name: 'rule',
       component: rule,
